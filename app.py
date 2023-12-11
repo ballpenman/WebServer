7c96f4942812005c4ee17ffbe64e3793a3e36e18
ballpenman <qhfvpsdls@gmail.com>
2023-12-12 00:42:24 +0900

    랭킹 시스템 구현 성공

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    regdate = db.Column(db.DateTime,default=datetime.now())
+
+with app.app_context():
+    db.create_all()
+
+name = ""
+# score = 4
+
+@app.route('/',methods=['GET'])
 def index():
-    return render_template("index.html")
+    if(name!= ""):
+        user = User(name=name,score=score)
+        db.session.add(user)
+        db.session.commit()
+    user_list = User.query.order_by(User.score.desc())
+    print("b")
+    return render_template('index.html',user_list=user_list)
+
+@app.route('/',methods=['POST'])
+def post():
+    name = request.args.get('name')
+    score = request.args.get('score')
+    user = User(name=name,score=score)
+    db.session.add(user)
+    db.session.commit()
+    return '처리 완료'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
